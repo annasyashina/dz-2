08beeb7564bc88ed18d62734f6b2712a54d606b6
sowa <sowa84@bk.ru>
2026-01-08 13:35:31 +0400

    Функции

--- main.go
+++ main.go
@@ -10,11 +10,19 @@ import (
 )
 
 func main() {
+	numbers := []int{}
 
-	operation := []string{}
+	/*operation := []string{}
 	operation = append(operation, "AVG")
 	operation = append(operation, "SUM")
 	operation = append(operation, "MED")
+	*/
+
+	operation := map[string]func([]int) float64{
+		"AVG": avg,
+		"SUM": sum,
+		"MED": med,
+	}
 
 	//var str string
 	fmt.Println("Введите строку неограниченное число чисел через запятую")
@@ -32,7 +40,7 @@ func main() {
 	fmt.Println(str)
 	parts := strings.Split(str, ",")
 	fmt.Println(parts)
-	numbers := []int{}
+
 	for i := 0; i < len(parts); i++ {
 		if value, err := strconv.Atoi(parts[i]); err == nil {
 			//fmt.Printf("%q looks like a number.\n", parts[i])
@@ -47,15 +55,21 @@ func main() {
 		fmt.Println("Введите операцию", operation, ":")
 		fmt.Scan(&sourceOperation)
 		//	fmt.Println(sourceOperation)
-		if elementInArray(sourceOperation, operation) {
-			isOperation = true
+		//if elementInArray(sourceOperation, operation) {
+		//	isOperation = true
+		//}
+
+		handler, ok := operation[sourceOperation] // Проверяем, существует ли ключ
+		if ok {
+			handler(numbers) // Вызываем найденную функцию
+			fmt.Println(sourceOperation, ":", handler(numbers))
+		} else {
+			fmt.Printf("Команда '%s' не найдена\n", sourceOperation)
 		}
 	}
-
-	fmt.Println(sourceOperation, ":", calculate(sourceOperation, numbers))
 }
 
-func elementInArray(element string, elements []string) bool {
+/*func elementInArray(element string, elements []string) bool {
 	//fmt.Println("elementinarray")
 	for i := 0; i < len(elements); i++ {
 		if elements[i] == element {
@@ -64,8 +78,40 @@ func elementInArray(element string, elements []string) bool {
 	}
 	return false
 }
+*/
 
-func calculate(sourceOperation string, numbers []int) float64 {
+func sum(numbers []int) float64 {
+	var sum int
+	for _, number := range numbers {
+		sum += number
+	}
+	return float64(sum)
+}
+
+func avg(numbers []int) float64 {
+	var sum int
+	for _, number := range numbers {
+		sum += number
+	}
+	return float64(sum) / float64(len(numbers))
+}
+
+func med(numbers []int) float64 {
+	sort.Ints(numbers)
+	mNumber := len(numbers) / 2
+	result := 0.0
+	if len(numbers)%2 == 0 {
+		result = (float64(numbers[mNumber-1]) + float64(numbers[mNumber])) / 2
+
+	} else {
+
+		result = float64(numbers[mNumber])
+	}
+
+	return result
+}
+
+/*func calculate(sourceOperation string, numbers []int) float64 {
 	result := 0.0
 	switch sourceOperation {
 	case "AVG":
@@ -94,3 +140,4 @@ func calculate(sourceOperation string, numbers []int) float64 {
 	}
 	return result
 }
+*/
